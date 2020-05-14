133fb4039e7df74e1d7659b01dba469d6b92da01
Andres <andresguerra0625@gmail.com>
2020-05-13 19:02:10 -0500

    first commit

---
+++ src/controlador/ControladorVistaPrincipal.java
@@ -0,0 +1,20 @@
+package controlador;
+
+import javafx.event.ActionEvent;
+
+public class ControladorVistaPrincipal {
+    public void mnuAgregar_action(ActionEvent actionEvent) {
+    }
+
+    public void mnuListasPrincipales_action(ActionEvent actionEvent) {
+    }
+
+    public void mnuListasIntersectadas_action(ActionEvent actionEvent) {
+    }
+
+    public void mnuListasExcluyentes_action(ActionEvent actionEvent) {
+    }
+
+    public void mnuLZ_action(ActionEvent actionEvent) {
+    }
+}
